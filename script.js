bc43ee2301bca3a2c051ba57cdfd5d62a424bf2f
Samuel Simplicio Valentim <samuelsimplicio@poli.ufrj.br>
2021-01-14 10:32:08 -0300

    Table test

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTable() {
+    var myTableDiv = document.getElementById("channels");
+  
+    var table = document.createElement('TABLE');
+  
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+  
+    for (var i = 0; i < 3; i++) {
+      var tr = document.createElement('TR');
+      tableBody.appendChild(tr);
+  
+      for (var j = 0; j < 4; j++) {
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode("Cell " + i + "," + j));
+        tr.appendChild(td);
+      }
+    }
+    myTableDiv.appendChild(table);
+  }
+  addTable();
